d4a52b5c6a3451b6496c5c7e53ef6b36e9cbfac0
Jonathan White <33507081+jonathan-white@users.noreply.github.com>
2020-08-24 23:53:16 -0400

    title update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 365 Cards for Hospitalized Kids
 Visit http://www.cardsforhospitalizedkids.com/ to find ways you can get involved.
+
+## Getting Started
+
+```
+$ npm start
+```
--- controllers/cardsController.js
+++ controllers/cardsController.js
@@ -1,8 +1,8 @@
-const db = require("../models");
+const { Card } = require("../models");
 
 module.exports = {
   findAll: function(req, res) {
-    db.Card
+    Card
       .find(req.query)
       .sort({ dateAdded: 1 })
       .then(dbModel => res.json(dbModel))
@@ -12,29 +12,29 @@ module.exports = {
       });
   },
   findCard: function(req, res) {
-    db.Card
+    Card
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   addCard: function(req, res) {
-    db.Card
+    Card
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Card
+    Card
       .findOneAndUpdate({ _id: req.params.id }, req.body)
 			.then(dbModel => 
-				db.Card.findById(dbModel._id)
+				Card.findById(dbModel._id)
 					.then(dbCard => res.json(dbCard))
 					.catch(err => res.status(422).json(err))	
 			)
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Card
+    Card
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
